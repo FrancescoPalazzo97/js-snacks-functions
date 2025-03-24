220a64f9cbd59c657234852f4511075e75d29fb6
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-24 16:07:28 +0100

    [UPD] 4th exercise done + bonus

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,30 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
-
+// function contaVocali (stringa){
+//     let conta = 0;
+//     for (let i = 0; i < stringa.length; i++){
+//         if ((stringa[i] === `a`) || (stringa[i] === `e`) || (stringa[i] === `i`) || (stringa[i] === `o`) || (stringa[i] === `u`)){
+//             conta += 1;
+//         }
+//     }
+//     return conta;
+// }
+//BONUS
+const contaVocali = stringa =>{
+    let conta = 0;
+        for (let i = 0; i < stringa.length; i++){
+            if ((stringa[i] === `a`) || (stringa[i] === `e`) || (stringa[i] === `i`) || (stringa[i] === `o`) || (stringa[i] === `u`)){
+                conta += 1;
+            }
+        }
+    return conta;
+ }
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let nVocali = contaVocali(word);
+console.log(`Le vocali nella parolsa sono: ${nVocali}`);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
