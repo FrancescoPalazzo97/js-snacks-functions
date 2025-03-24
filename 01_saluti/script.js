cb9986ab9465d1af4ab9125b42a8317ac201b3b7
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-24 14:49:06 +0100

    [UPD] 1st exercise done

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function hiUser (stringa) {
+    console.log(`Ciao ${stringa}`);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+hiUser (userName);
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
