6fdeb549d84c9d0086920b2d2a892388f9777311
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-24 15:10:06 +0100

    [UPD] 2nd exercise done + bonus

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+// function firstChar (arr) {
+//     let letter = [];
+//     for (let i = 0; i < arr.length; i++){
+//         letter.push(arr[i].charAt(0));
+//     }
+//     return letter
+// }
+//BONUS
+const firstChar = arr => {
+    let letter = [];
+    for (let i = 0; i < arr.length; i++){
+        letter.push(arr[i].charAt(0));
+    }
+    return letter
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let firstLetter = firstChar(names);
+console.log(firstLetter);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
