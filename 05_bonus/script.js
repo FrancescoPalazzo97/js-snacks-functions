5225e158382ce45b0709878b93263e128fe22da6
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-24 16:30:18 +0100

    [DONE] 5th exercise(bonus) done + bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,30 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+// function hiUser(user, hour){
+//     if (hour >= 0 && hour <= 13){
+//         console.log(`Buongiorno ${user}`);
+//     } else if (hour > 13 && hour <= 17){
+//         console.log(`Buon pomeriggio ${user}`);
+//     } else {
+//         console.log(`Buonasera ${user}`);
+//     }
+// }
+//BONUS
+const hiUser = (user, hour) =>{
+    if (hour >= 0 && hour <= 13){
+        console.log(`Buongiorno ${user}`);
+    } else if (hour > 13 && hour <= 17){
+        console.log(`Buon pomeriggio ${user}`);
+    } else {
+        console.log(`Buonasera ${user}`);
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let time = new Date();
+let hours = time.getHours();
+hiUser (name, hours);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
