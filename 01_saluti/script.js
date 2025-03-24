ea22f1f186d7d811a31f5e569814f7f4ce22f60e
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-24 14:52:29 +0100

    [UPD] Bonus 1st exercise done

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,9 +4,11 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-function hiUser (stringa) {
-    console.log(`Ciao ${stringa}`);
-}
+// function hiUser (stringa) {
+//     console.log(`Ciao ${stringa}`);
+// }
+//BONUS
+const hiUser = stringa => console.log(`Ciao ${stringa}`);
 
 // Invoca la funzione qui e stampa il risultato in console
 hiUser (userName);
